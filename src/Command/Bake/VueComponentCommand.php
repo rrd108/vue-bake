759093b561ff3e735393edd9e345f3ed97fee25e
rrd108 <rrd@webmania.cc>
2023-07-05 10:48:13 +0200

    get vars

--- src/Command/Bake/VueComponentCommand.php
+++ src/Command/Bake/VueComponentCommand.php
@@ -13,7 +13,7 @@ use Cake\Console\ConsoleOptionParser;
 class VueComponentCommand extends SimpleBakeCommand
 {
     public $pathFragment = 'VueComponents/';
-    private $templateCommand;
+    protected $modelName;
 
     public function __construct()
     {
@@ -56,13 +56,80 @@ class VueComponentCommand extends SimpleBakeCommand
             $this->pathFragment = rtrim($args->getArgument('path'), '/') . '/';
         }
 
+        $this->modelName = $args->getArgument('name');
+
         return parent::execute($args, $io);
     }
 
     public function templateData(Arguments $arguments): array
     {
         $lang = $arguments->getArgument('lang');
+        $vars = $this->_loadController();
 
-        return ['lang' => $lang];
+        return [
+            'lang' => $lang,
+        ];
+    }
+
+    // This is TemplateCommand's _loadController's copy
+    protected function _loadController(): array
+    {
+        if ($this->getTableLocator()->exists($this->modelName)) {
+            $modelObject = $this->getTableLocator()->get($this->modelName);
+        } else {
+            $modelObject = $this->getTableLocator()->get($this->modelName, [
+                'connectionName' => $this->connection,
+            ]);
+        }
+
+        /*$primaryKey = $displayField = $singularVar = $singularHumanName = null;
+        $schema = $fields = $hidden = $modelClass = null;
+        try {
+            $primaryKey = (array)$modelObject->getPrimaryKey();
+            $displayField = $modelObject->getDisplayField();
+            $singularVar = $this->_singularName($this->controllerName);
+            $singularHumanName = $this->_singularHumanName($this->controllerName);
+            $schema = $modelObject->getSchema();
+            $fields = $schema->columns();
+            $hidden = $modelObject->newEmptyEntity()->getHidden() ?: ['token', 'password', 'passwd'];
+            $modelClass = $this->modelName;
+        } catch (\Exception $exception) {
+            $io->error($exception->getMessage());
+            $this->abort();
+        }
+
+        [, $entityClass] = namespaceSplit($this->_entityName($this->modelName));
+        $entityClass = sprintf('%s\Model\Entity\%s', $namespace, $entityClass);
+        if (!class_exists($entityClass)) {
+            $entityClass = EntityInterface::class;
+        }
+        $associations = $this->_filteredAssociations($modelObject);
+        $keyFields = [];
+        if (!empty($associations['BelongsTo'])) {
+            foreach ($associations['BelongsTo'] as $assoc) {
+                $keyFields[$assoc['foreignKey']] = $assoc['variable'];
+            }
+        }
+
+        $pluralVar = Inflector::variable($this->controllerName);
+        $pluralHumanName = $this->_pluralHumanName($this->controllerName);*/
+
+        return compact(
+            'modelObject',
+            'modelClass',
+            'entityClass',
+            'schema',
+            'primaryKey',
+            'displayField',
+            'singularVar',
+            'pluralVar',
+            'singularHumanName',
+            'pluralHumanName',
+            'fields',
+            'hidden',
+            'associations',
+            'keyFields',
+            'namespace'
+        );
     }
 }
