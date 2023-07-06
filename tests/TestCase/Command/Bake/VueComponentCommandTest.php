48f5c222c0932335a31b7f4cdea597ffd9ffbd8d
rrd108 <rrd@webmania.cc>
2023-07-06 13:59:48 +0200

    test passes

--- tests/TestCase/Command/Bake/VueComponentCommandTest.php
+++ tests/TestCase/Command/Bake/VueComponentCommandTest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace VueBake\Test\Command\Bake;
 
+use Cake\Filesystem\Folder;
 use Cake\TestSuite\TestCase;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
 use Cake\Datasource\ConnectionManager;
 use Cake\Console\TestSuite\ConsoleIntegrationTestTrait;
 
@@ -20,8 +23,7 @@ class VueComponentCommandTest extends TestCase
     {
         parent::setUp();
         $this->useCommandRunner();
-        // Sets the TestApp namespace to be used instead of App
-        $this->setAppNamespace();
+        $this->setAppNamespace('VueBake\Test\App');
         $this->configApplication(
             'TestApp\Application',
             [PLUGIN_TESTS . 'test_app' . DS . 'config']
@@ -31,6 +33,7 @@ class VueComponentCommandTest extends TestCase
     public function tearDown(): void
     {
         parent::tearDown();
+        $this->removeTestGeneratedFiles(PLUGIN_TESTS . '..' . DS . 'TestApp');
     }
 
     public function testExecute(): void
@@ -38,9 +41,27 @@ class VueComponentCommandTest extends TestCase
         $this->exec('bake vue_component Posts --lang ts');
         $this->assertExitSuccess();
         // Assert the generated files exist in the expected path
-        $this->assertFileExists(ROOT . DS . 'src' . DS . 'VueComponents' . DS . 'PostsIndex.vue');
-        $this->assertFileExists(ROOT . DS . 'src' . DS . 'VueComponents' . DS . 'PostsAdd.vue');
-        $this->assertFileExists(ROOT . DS . 'src' . DS . 'VueComponents' . DS . 'PostsEdit.vue');
-        $this->assertFileExists(ROOT . DS . 'src' . DS . 'VueComponents' . DS . 'PostsView.vue');
+        $this->assertFileExists(PLUGIN_TESTS . '..' . DS . 'TestApp' . DS . 'VueComponents' . DS . 'PostsIndex.vue');
+        $this->assertFileExists(PLUGIN_TESTS . '..' . DS . 'TestApp' . DS . 'VueComponents' . DS . 'PostsAdd.vue');
+        $this->assertFileExists(PLUGIN_TESTS . '..' . DS . 'TestApp' . DS . 'VueComponents' . DS . 'PostsEdit.vue');
+        $this->assertFileExists(PLUGIN_TESTS . '..' . DS . 'TestApp' . DS . 'VueComponents' . DS . 'PostsView.vue');
+    }
+
+    private function removeTestGeneratedFiles($directoryPath)
+    {
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($directoryPath, RecursiveDirectoryIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($iterator as $file) {
+            if ($file->isDir()) {
+                rmdir($file->getRealPath());
+            } else {
+                unlink($file->getRealPath());
+            }
+        }
+
+        rmdir($directoryPath);
     }
 }
