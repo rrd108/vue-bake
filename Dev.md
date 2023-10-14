c43c0c655717183e57fdb4d41c1d4cec496e97eb
rrd108 <rrd@webmania.cc>
2023-10-14 09:53:03 +0200

    fix #7

---
+++ Dev.md
@@ -0,0 +1,9 @@
+# VueBake plugin for CakePHP Dev Info
+
+## Run the tests
+
+From the plugin's folder execute
+
+```bash
+vendor/bin/phpunit tests
+```
--- tests/TestCase/Command/Bake/VueComponentCommandTest.php
+++ tests/TestCase/Command/Bake/VueComponentCommandTest.php
@@ -117,7 +117,23 @@ class VueComponentCommandTest extends TestCase
         $this->assertStringContainsString('.post(`${import.meta.env.VITE_APP_API_URL}post.json`, post.value)', $content);
         $this->assertStringContainsString('<form @submit.prevent=createNewPost>', $content);
         $this->assertStringContainsString('<label>title</label>', $content);
-        $this->assertStringContainsString('<input type="text" v-model={{ post.title }} />', $content);
+        $this->assertStringContainsString('<input type="text" v-model="post.title" />', $content);
+    }
+
+    public function testExecuteEditFileContentTs()
+    {
+        $this->exec('bake vue_component Posts --lang ts');
+        $this->assertExitSuccess();
+        $content = file_get_contents(PLUGIN_TESTS . '..' . DS . 'TestApp' . DS . 'VueComponents' . DS . 'PostsEdit.vue');
+        $this->assertStringContainsString('<script setup lang="ts">', $content);
+        $this->assertStringContainsString('import Post from \'@/types/Post\'', $content);
+
+        $this->assertStringContainsString('const post = ref<Post>()', $content);
+        $this->assertStringContainsString('const editPost = () => {', $content);
+        $this->assertStringContainsString('.put(`${import.meta.env.VITE_APP_API_URL}post.json`, post.value)', $content);
+        $this->assertStringContainsString('<form @submit.prevent=editPost>', $content);
+        $this->assertStringContainsString('<label>title</label>', $content);
+        $this->assertStringContainsString('<input type="text" v-model="post.title" />', $content);
     }
 
     private function removeTestGeneratedFiles($directoryPath)
